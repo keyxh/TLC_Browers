391057292271b8e9acb51b42d30897249cb8015d
wen-hui <91294497+keyxh@users.noreply.github.com>
2023-05-13 14:23:01 +0800

    Delete 一键reg.bat

--- 一键reg.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d %~dp0 
-regsvr32 /S dll\file_controlv2.dll
-regsvr32 /S dll\mathv3.dll
-regsvr32 /S dll\Windows_FormApi.dll
-regsvr32 /S dll\RC6.dll
-start /wait dll\runtime_install.exe 
-pause
-exit
